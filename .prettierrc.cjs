c8cfdf932edf2bab36ff648a7aac3c92fbde820b
Radu-Cristian Popa <radu.popa@breakpointit.eu>
2023-05-08 11:13:48 +0300

    initial commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    quoteProps: 'consistent',
+    jsxSingleQuote: false,
+    trailingComma: 'none'
+}
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <div className="flex items-center justify-center text-white h-screen">
+            <div className="relative z-10">
+                <div className="mt-1.5 inline-block rounded-lg outline-none bg-green-700 cursor-pointer">
+                    <span className="font-bold uppercase flex -translate-y-1.5 items-center rounded-lg px-5 py-3 text-center text-white drop-shadow-[0_35px_35px_rgba(0,255,0,0.25)] bg-green-500 duration-300 active:-translate-y-1 active:drop-shadow-[0_35px_50px_rgba(0,255,0,0.25)]">
+                        Collard greens
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App
